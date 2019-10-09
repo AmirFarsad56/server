2e9a18e8a6b8469aa5dff492b9ae011418b460f7
AmirFarsad56 <AmirFarsad56@outlook.com>
2019-10-09 14:33:09 +0330

    OCT 9

--- Main_Project/settings.py
+++ Main_Project/settings.py
@@ -29,7 +29,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'statics')
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [config['ALLOWED_HOST'],]
 
